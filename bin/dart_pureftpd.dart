d05fd7555b7a7907a31625277e48643bbf272126
francois <admin@purpleguy.ovh>
2022-05-18 11:16:18 +0200

    menu v1

--- bin/dart_pureftpd.dart
+++ bin/dart_pureftpd.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
   titre();
   menu();
@@ -9,9 +11,31 @@ void titre() {
   print("");
 }
 
+void revoir() {
+  print("");
+  print("au revoir");
+  print("");
+}
+
 void menu() {
-  affichmenu();
-  selectmenu();
+  bool STOP = false;
+  do {
+    affichmenu();
+    int choix = choixMenu(4);
+
+    if (choix == 0) {
+      print("quitter");
+      STOP = true;
+    } else if (choix == 1) {
+      print("mise a jour des dépôt");
+    } else if (choix == 2) {
+      print("mise a jour des pâquet");
+    } else if (choix == 3) {
+      print("instalation de pure-ftp");
+    } else if (choix == 4) {
+      print("ajout d'un utilisateur");
+    }
+  } while (STOP != true);
 }
 
 void affichmenu() {
@@ -27,7 +51,21 @@ void affichmenu() {
   print("+-----------------------------------+");
 }
 
-void selectmenu() {
-  print("");
-  print("sélectionner une veleur entre 0 et 4 :");
+int choixMenu(int nbChoix) {
+  bool saisieValide = false;
+  int i = -1;
+  while (!saisieValide) {
+    print("> Veuillez saisir une action (0-$nbChoix)");
+    try {
+      i = int.parse(stdin.readLineSync().toString());
+      if (i >= 0 && i <= nbChoix) {
+        saisieValide = true;
+      } else {
+        print("La saisie ne correspond à aucune action.");
+      }
+    } catch (e) {
+      print("Erreur dans la saisie.");
+    }
+  }
+  return i;
 }
